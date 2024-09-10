9a92def1647cf2b63a784d368532bab07a89cae7
Azuriru <kegumine@gmail.com>
2024-09-10 16:19:41 +0800

    Patch errors

--- src/lib/i18n/index.ts
+++ src/lib/i18n/index.ts
@@ -1,5 +1,5 @@
 import { dev } from '$app/environment';
-import i18n, { Config } from 'sveltekit-i18n';
+import i18n, { type Config } from 'sveltekit-i18n';
 import lang from './lang.json';
 
 export const defaultLocale = 'en';
